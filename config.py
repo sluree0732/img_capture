99303c008e81e8aa651cfb846436a3e8d8f7c4c8
vellygom <fluree0732@naver.com>
2026-06-23 14:16:07 +0900

    feat: 셀렉터/URL/타임아웃 상수 정의

---
+++ config.py
@@ -0,0 +1,13 @@
+LOGIN_URL = "https://www.instagram.com/accounts/login/"
+
+USERNAME_INPUT_SELECTOR = "input[name='username']"
+PASSWORD_INPUT_SELECTOR = "input[name='password']"
+LOGIN_BUTTON_SELECTOR = "button[type='submit']"
+
+POST_LINK_SELECTOR = "article a[href*='/p/']"
+POST_DETAIL_PANEL_SELECTOR = "article"
+
+PAGE_LOAD_TIMEOUT_SEC = 20
+SCROLL_PAUSE_SEC = 2
+LOGIN_POLL_INTERVAL_SEC = 2
+LOGIN_POLL_TIMEOUT_SEC = 180
